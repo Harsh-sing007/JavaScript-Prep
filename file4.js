aa5288d083fe7ffffc521c5f98dc859a6c419490
Harsh <harshsingh45831@gmail.com>
2026-02-11 12:24:13 +0530

    update

--- file4.js
+++ file4.js
@@ -1,13 +1,15 @@
 let a = 10
 let b = 5
 //if(a>b){
-    //console.log("a is greater than ");
+    //console.log("a is greater");
 //}else{
-    //console.log("b is greater than ");
+    //console.log("b is greater  ");
 //}
 
 if (a > b) 
     console.log("a is greater");
-else 
+else if (b > a)
     console.log("b is greater");
+else
+    console.log("a and b are equal");
 
