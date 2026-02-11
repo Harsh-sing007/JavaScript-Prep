15cfa69f757a47f5c1c79feed2d6f8aefe72ab66
Harsh <harshsingh45831@gmail.com>
2026-02-11 16:09:34 +0530

    update

---
+++ file13.js
@@ -0,0 +1,9 @@
+// let item = () => {
+//     return 10
+// };
+// const result = item();
+// console.log(result);
+
+const items = ()  => ({name: "John"})
+const results = items();
+console.log(result);
