eb521eafb22f03369a215550c9d58d449593e81b
Harsh <harshsingh45831@gmail.com>
2026-02-11 16:11:31 +0530

    update

--- file14.js
+++ file14.js
@@ -1,9 +1,9 @@
 //array
 
 const names = ["Vivek","Shivam","Aman"]
-console.log(names)
-console.log(names[0])
-names.push("Suresh")
-console.log(names)
+// console.log(names)
+// console.log(names[0])
+// names.push("Suresh")
+// console.log(names)
 const arr = [...names,"Suresh"]
 console.log(arr)
