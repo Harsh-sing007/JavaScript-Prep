4d875c0f54d70d523225c8063094ccde3ee04da6
Harsh <harshsingh45831@gmail.com>
2026-02-13 11:43:52 +0530

    update

--- file28.js
+++ file28.js
@@ -1,3 +1,9 @@
+// const names =["John", "Cathy", "Ammy"]
+// names.forEach((name,index,arr) =>{
+//     console.log(arr[index]);
+// })
+
+
 const names =["John", "Cathy", "Ammy"]
 const score =[ 56,78,90 ]
 
