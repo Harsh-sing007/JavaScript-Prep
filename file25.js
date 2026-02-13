66598b5fb638669f325aecea3c4ecd76612b9d70
Harsh <harshsingh45831@gmail.com>
2026-02-13 10:25:58 +0530

    IIFE

---
+++ file25.js
@@ -0,0 +1,11 @@
+// function greet(){
+// console.log(("Hello World"));   
+// }
+// greet();
+
+//IIFE - Immediately Invoked Function Expression
+//A function that is defined and executed immediately after its creation. It is often used to create a new scope and avoid polluting the global namespace.
+(function greet(){
+console.log(("Hello World"));   
+})();
+
