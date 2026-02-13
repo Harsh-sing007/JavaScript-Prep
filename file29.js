7e7723ee9ef34b550c74b4c5cca6b485ceb751ed
Harsh <harshsingh45831@gmail.com>
2026-02-13 15:49:56 +0530

    updated

--- file29.js
+++ file29.js
@@ -22,10 +22,17 @@
 // const result = add(9, 5, 7, 8);
 // console.log(result);
 
-function add(...arr)
-{
-    let sum = arr.reduce((total, num) => total+num, 0);
-    return sum;
-}
-const result = add(9, 5, 7, 8);
+// function add(...arr)
+// {
+//     let sum = arr.reduce((total, num) => total+num, 0);
+//     return sum;
+// }
+// const result = add(9, 5, 7, 8);
+// console.log(result);
+
+const add = (...args) => {
+   let sum = args.reduce((total, num) => total+num, 0);
+   return sum;
+};
+const result = add(9, 5, 7, 8); 
 console.log(result);
