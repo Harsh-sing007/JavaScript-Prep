9463345cde3886ee85a106a810b1b2628bf7deb5
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-02-15 23:44:37 +0530

    Refactor result calculations using ternary and logical operators

--- file27.js
+++ file27.js
@@ -8,15 +8,18 @@ else{
 }
 console.log(result);
 
+
 //Ternary operator
 //let score = 90  
 let result2 = score > 50 ? "Passed" : "Failed";
 console.log(result2);
 
+
 //And  operators
-let score = 9
+//let score = 9
 let result3 = score > 50 && "Passed";
 console.log(result3); // false
+
 
 //Or operators
 //let score = 9   
