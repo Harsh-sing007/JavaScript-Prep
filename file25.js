98378a5b1dc04cd5be1c7083cdb3d577db0fb6bd
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-02-15 23:43:38 +0530

    Implement IIFE for greeting message
    Added an Immediately Invoked Function Expression (IIFE) to greet.

--- file25.js
+++ file25.js
@@ -5,6 +5,7 @@
 
 //IIFE - Immediately Invoked Function Expression
 //A function that is defined and executed immediately after its creation. It is often used to create a new scope and avoid polluting the global namespace.
+//used in js
 (function greet(){
 console.log(("Hello World"));   
 })();
