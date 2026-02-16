6f08a09e15863ab9995a7e162f8ef40aa2e2482a
Harsh <harshsingh45831@gmail.com>
2026-02-16 13:12:32 +0530

    string method

--- file37.js
+++ file37.js
@@ -1,9 +1,17 @@
-let str ="Hello"
+
+
+
+
+//string methods
+let str="Hello"
 // console.log(str.toUpperCase())
 // console.log(str.toLowerCase())
 // console.log(str.indexOf("l"))
 // console.log(str.lastIndexOf("l"))
 // console.log(str.includes("w"))
 // console.log(str.startsWith("H"))
-// console.log(str.endsWith("d"))
+// console.log(str.endsWith("u"))
+
 console.log(str.slice(1,4))
+console.log(str.substring(1,4))
+//both works the same
