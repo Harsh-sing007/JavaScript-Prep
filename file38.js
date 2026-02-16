7f6a723f701a5fca31bb70d4193982f6f0ecedbe
Harsh <harshsingh45831@gmail.com>
2026-02-16 13:11:27 +0530

    update

---
+++ file38.js
@@ -0,0 +1,30 @@
+// // //dates
+// let d=new Date()
+// console.log(d)
+//prints current date
+
+//1-1-1970 starting point
+
+//get methods
+// console.log(d.getDate())
+// console.log(d.getMonth())
+// console.log(d.getFullYear())
+// console.log(d.getHours())
+// console.log(d.getMinutes())
+// console.log(d.getSeconds())
+// console.log(d.getMilliseconds())
+// console.log(d.getDay())
+//prints day of the week 
+//console.log(d.getTime())
+//prints a serial number
+//console.log(Date.now())
+
+
+// let d=new Date(2026,1,1)
+// let d=new Date("February 16,2026 10:10:10")
+//let d=new Date(2026,1,1,10,10,10)
+let d=new Date("16Febr 2024 10:10:10")
+//let d=new Date("16hdvhdbv,2026 10:10:10")
+//junk
+console.log(d)
+console.log(d.getMonth())
