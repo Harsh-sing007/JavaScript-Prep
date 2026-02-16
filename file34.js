d569f5447f854e3e116a1d7ea7781e6f6ec2b365
Harsh <harshsingh45831@gmail.com>
2026-02-16 10:50:24 +0530

    updates

--- file34.js
+++ file34.js
@@ -1,9 +1,22 @@
-function response(user){
-    console.log(user)
+// function response(user){
+//     console.log(user)
+// }
+//     const user={
+//         name:"John",
+//         email:"john@gmail.com",
+//         role:"user"
+//     };
+//     response (user)
+
+
+function response({name,email,role})
+{
+    console.log(name)
+    console.log(email)
 }
-    const user={
-        name:"John",
-        email:"john@gmail.com",
-        role:"user"
-    };
-    response (user)
+const user ={
+    name:"John",
+    email:"john@email.com",
+    role:"user"
+};
+response(user)
