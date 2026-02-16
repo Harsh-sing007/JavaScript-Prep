b1d83bb80923cae1fae7709eafb20cd3b9789fa5
Harsh <harshsingh45831@gmail.com>
2026-02-16 11:19:44 +0530

    update

---
+++ file35.js
@@ -0,0 +1,12 @@
+// const names =["Harsh", " Akhil", "Mohan", "Suresh"]
+const names = new Array("Harsh", " Akhil", "Mohan", "Suresh")
+//names.push("Vivek")
+//names.pop()
+// // names.shift()
+// names.unshift()
+//shift and unshift is slower because of rearrangement 
+// console.log(names)
+// console.log(names.sort())
+// console.log(names.reverse())
+const numbers = [5,2,1,8,3]
+// console.log(numbers.sort())
