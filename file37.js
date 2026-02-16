d47f2f1348992dbf5f400e6e990d056ea6ee0aed
Harsh <harshsingh45831@gmail.com>
2026-02-16 11:34:19 +0530

    update

---
+++ file37.js
@@ -0,0 +1,9 @@
+let str ="Hello"
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// console.log(str.indexOf("l"))
+// console.log(str.lastIndexOf("l"))
+// console.log(str.includes("w"))
+// console.log(str.startsWith("H"))
+// console.log(str.endsWith("d"))
+console.log(str.slice(1,4))
