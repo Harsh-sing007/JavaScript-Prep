97b863200794c83dcca2536730b99a5a07c3790b
Harsh <harshsingh45831@gmail.com>
2026-02-17 11:33:05 +0530

    build async promotion system with eligibility checks and error handling

---
+++ file48.js
@@ -0,0 +1,7 @@
+const students = [
+        {id: 1 , name: "Akhil", s1: true, s2: true, s3: false},
+        {id: 2 , name: "Harsh", s1: true, s2: true, s3: true},
+        {id: 3 , name: "Aryajeet", s1: false, s2: true, s3: true},
+];
+
+//if id is 1> akhil is absent 
