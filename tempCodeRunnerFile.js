e48d16184e26a2d1a26e9640a9e0738e0f8f7461
Harsh <harshsingh45831@gmail.com>
2026-02-17 14:13:23 +0530

    updATE

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-async function main()
-// { try{ const result = await Promise.any([p1(), p2(), p3()]);   //p1 prints because of time
-//     console.log(result)
-// } catch(e)
-// {console.log(e)}
-// }
-    
-
-// main()
+console.log(student)
