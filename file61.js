054c461a057c13c91c4a52e6bda18ef81d030f39
Harsh <harshsingh45831@gmail.com>
2026-02-18 14:50:17 +0530

    update

---
+++ file61.js
@@ -0,0 +1,49 @@
+// const emp1 = {
+//     name: "John",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+// emp1.display()
+
+
+// const emp1 = {
+//     name: "John",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+// const emp2 = {
+//     name: "Cathy",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+// emp1.display()
+// emp2.display()
+
+// function Employee(name) {
+//     this.name = name;
+
+//     this.display = function () {
+//         console.log(this.name);
+//     };
+// }
+
+// const emp1 = new Employee("John");
+// emp1.display();
+// const emp2 = new Employee("Jack");
+// emp2.display();
+
+//prototype
+function Employee(name){
+    this.name = name;
+}
+Employee.prototype.display = function() {
+    console.log(this.name)
+};
+const emp1 = new Employee("John");
+emp1.display();
+const emp2 = new Employee("Jack");
+emp2.display();
+
