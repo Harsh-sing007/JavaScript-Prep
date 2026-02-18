c556162b91eb3f145b909cbaa2804b988359f341
Harsh <harshsingh45831@gmail.com>
2026-02-18 14:28:42 +0530

    this keyword cart problem

---
+++ file60.js
@@ -0,0 +1,15 @@
+const cart = {
+    items: [],
+
+    addItems(item) {
+        this.items.push(item);
+    },
+
+    showItems() {
+        console.log(this.items);
+    }
+};
+
+cart.addItems("Laptop");
+cart.addItems("Desktop");
+cart.showItems();
